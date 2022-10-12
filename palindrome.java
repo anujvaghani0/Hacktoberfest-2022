77b7f54741424def48b97629397fc02c2a2b778e
DHARMIK  GANGANI <dharmikgangani786@gmail.com>
2022-10-12 20:07:34 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,22 +1,18 @@
-package Arrays;
-
-public class palindrome {
-    public static void main(String[] args) {
-        int n = -121;
-        System.out.println(palindrome(n));
-    }
-
-    public static boolean palindrome(int n) {
-        int ans = 0;
-        int temp = n;
-        while (n > 0) {
-            int r = n % 10;
-            ans = (ans * 10) + r;
-            n = n / 10;
-        }
-        if (ans == temp) {
-            return true;
-        }
-        return false;
-    }
-}
+import java.util.*;   
+class PalindromeExample2  
+{  
+   public static void main(String args[])  
+   {  
+      String original, reverse = ""; // Objects of String class  
+      Scanner in = new Scanner(System.in);   
+      System.out.println("Enter a string/number to check if it is a palindrome");  
+      original = in.nextLine();   
+      int length = original.length();   
+      for ( int i = length - 1; i >= 0; i-- )  
+         reverse = reverse + original.charAt(i);  
+      if (original.equals(reverse))  
+         System.out.println("Entered string/number is a palindrome.");  
+      else  
+         System.out.println("Entered string/number isn't a palindrome.");   
+   }  
+}  
