dd2e0fdda5e6fdb592f5a9065121fc2d3be77e93
190420116064 <95023117+190420116064@users.noreply.github.com>
2022-10-12 14:30:59 +0530

    Update MaximumSubarray.java

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -2,7 +2,7 @@ package Arrays;
 
 public class MaximumSubarray {
     public static void main(String[] args) {
-        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] nums = {-2, 6, -3, 4, -1, 2, 1, -5, 4};
         System.out.println(maxSubArray(nums));
     }
 
