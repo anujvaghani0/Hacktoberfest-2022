e8bac4debe6f17ae9ec8b9addb9b0fdfe85b11bd
Abhisb1304 <104093113+Abhisb1304@users.noreply.github.com>
2022-10-29 23:38:06 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -2,7 +2,7 @@ package Arrays;
 
 public class factorial {
     public static void main(String[] args) {
-        System.out.println(factorial(6));
+        System.out.println(factorial(7));
     }
 
     public static int factorial(int n) {
