60b6aef1eb7976645f8026a607299f79395f2581
Vaibhav Dangar <vaibhavdangar20@gmail.com>
2023-10-04 17:47:36 +0530

    Update TwoOutOfThree.java

--- TwoOutOfThree.java
+++ TwoOutOfThree.java
@@ -11,6 +11,7 @@ public class TwoOutOfThree {
         int[] nums3 = {3};
         List<Integer> ans = twoOutOfThree(nums1, nums2, nums3);
         System.out.println(ans);
+        
     }
 
     public static List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
